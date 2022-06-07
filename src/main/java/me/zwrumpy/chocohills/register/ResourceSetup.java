0b24eace9e0e9f701241ad8615156798fa8ae70a
z <zwrumpydev@gmail.com>
2022-06-07 19:32:33 +0800

    0.33 shows recipe eggpensive and tinapang itlog

--- src/main/java/me/zwrumpy/chocohills/register/ResourceSetup.java
+++ src/main/java/me/zwrumpy/chocohills/register/ResourceSetup.java
@@ -18,56 +18,72 @@ public class ResourceSetup {
     }
 
     void setup() {
-        registerResource(ChocoItems.CHOCO_CRYSTAL, new ItemStack[]{
+        registerEnhanceCrafting(ChocoItems.CHOCO_CRYSTAL, new ItemStack[]{
                 new ItemStack(Material.NETHERITE_INGOT), null, new ItemStack(Material.NETHERITE_INGOT),
                 null, new ItemStack(Material.NETHER_STAR), null,
                 new ItemStack(Material.NETHERITE_INGOT), null, new ItemStack(Material.NETHERITE_INGOT)
         });
 
-        registerResource(ChocoItems.COMPRESSED_CRYSTAL, new ItemStack[]{
+        registerEnhanceCrafting(ChocoItems.COMPRESSED_CRYSTAL, new ItemStack[]{
                 ChocoItems.CHOCO_CRYSTAL, ChocoItems.CHOCO_CRYSTAL, ChocoItems.CHOCO_CRYSTAL,
                 ChocoItems.CHOCO_CRYSTAL, new ItemStack(Material.NETHER_STAR), ChocoItems.CHOCO_CRYSTAL,
                 ChocoItems.CHOCO_CRYSTAL, ChocoItems.CHOCO_CRYSTAL, ChocoItems.CHOCO_CRYSTAL
         });
 
-        registerResource(ChocoItems.HYPER_CRYSTAL, new ItemStack[]{
+        registerEnhanceCrafting(ChocoItems.HYPER_CRYSTAL, new ItemStack[]{
                 ChocoItems.COMPRESSED_CRYSTAL, ChocoItems.COMPRESSED_CRYSTAL, ChocoItems.COMPRESSED_CRYSTAL,
                 ChocoItems.COMPRESSED_CRYSTAL, new ItemStack(Material.NETHER_STAR), ChocoItems.COMPRESSED_CRYSTAL,
                 ChocoItems.COMPRESSED_CRYSTAL, ChocoItems.COMPRESSED_CRYSTAL, ChocoItems.COMPRESSED_CRYSTAL
         });
 
-        registerResource(ChocoItems.CHOCO_CRYSTAL_X2, new ItemStack[]{
+        registerEnhanceCrafting(ChocoItems.CHOCO_CRYSTAL_X2, new ItemStack[]{
                 ChocoItems.HYPER_CRYSTAL, null, ChocoItems.HYPER_CRYSTAL,
                 null, new ItemStack(Material.NETHER_STAR), null,
                 ChocoItems.HYPER_CRYSTAL, null, ChocoItems.HYPER_CRYSTAL
         });
 
-        registerResource(ChocoItems.ENERGIZED_CRYSTAL, new ItemStack[]{
+        registerEnhanceCrafting(ChocoItems.ENERGIZED_CRYSTAL, new ItemStack[]{
                 ChocoItems.CHOCO_CRYSTAL_X2, null, ChocoItems.CHOCO_CRYSTAL_X2,
                 SlimefunItems.ENERGIZED_CAPACITOR, new ItemStack(Material.NETHER_STAR), SlimefunItems.ENERGIZED_CAPACITOR,
                 ChocoItems.CHOCO_CRYSTAL_X2, null, ChocoItems.CHOCO_CRYSTAL_X2
         });
 
-        registerResource(ChocoItems.CHOCO_CARBON, new ItemStack[]{
+        registerEnhanceCrafting(ChocoItems.CHOCO_CARBON, new ItemStack[]{
                 ChocoItems.CHOCO_CRYSTAL, null, ChocoItems.CHOCO_CRYSTAL,
                 SlimefunItems.CARBON, ChocoItems.EGGPENSIVE, SlimefunItems.CARBON,
                 ChocoItems.CHOCO_CRYSTAL, null, ChocoItems.CHOCO_CRYSTAL
         });
 
-        registerResource(ChocoItems.AYANSHARD, new ItemStack[]{
+        registerEnhanceCrafting(ChocoItems.AYANSHARD, new ItemStack[]{
                 ChocoItems.CHOCO_CRYSTAL, new ItemStack(Material.AMETHYST_SHARD), ChocoItems.CHOCO_CRYSTAL,
                 new ItemStack(Material.AMETHYST_SHARD),  new ItemStack(Material.AMETHYST_SHARD),  new ItemStack(Material.AMETHYST_SHARD),
                 ChocoItems.CHOCO_CRYSTAL, new ItemStack(Material.AMETHYST_SHARD), ChocoItems.CHOCO_CRYSTAL
         });
 
-        registerResource(ChocoItems.GHOSTINGGOO, new ItemStack[]{
+        registerEnhanceCrafting(ChocoItems.GHOSTINGGOO, new ItemStack[]{
                 ChocoItems.CHOCO_CRYSTAL, SlimefunItems.STRANGE_NETHER_GOO, null,
                 SlimefunItems.STRANGE_NETHER_GOO, ChocoItems.AYANSHARD,  SlimefunItems.STRANGE_NETHER_GOO,
                 null, SlimefunItems.STRANGE_NETHER_GOO, ChocoItems.CHOCO_CRYSTAL
         });
+
+        registerNoCraftItem(ChocoItems.EGGPENSIVE, new ItemStack[]{
+                SlimefunItems.CARBON_CHUNK, ChocoItems.TINAPANG_ITLOG, null,
+                null, null, null,
+                null, null, null
+        });
+
+        registerNoCraftItem(ChocoItems.TINAPANG_ITLOG, new ItemStack[]{
+                new ItemStack(Material.EGG), null,
+                null, null, null,
+                null, null, null
+        });
     }
 
-    void registerResource(SlimefunItemStack stack, ItemStack[] recipe) {
+    void registerEnhanceCrafting(SlimefunItemStack stack, ItemStack[] recipe) {
         new UnplaceableBlock(ChocoItems.CH_RESOURCES, stack, RecipeType.ENHANCED_CRAFTING_TABLE, recipe).register(this.addon);
+    }
+
+    void registerNoCraftItem(SlimefunItemStack stack, ItemStack[] recipe) {
+        new UnplaceableBlock(ChocoItems.CH_RESOURCES, stack, RecipeType.NULL, recipe).register(this.addon);
     }
 }
